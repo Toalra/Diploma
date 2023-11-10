edfb5d0360e769f88a7f4c5f91df77998d26de06
Toalra <K0$k@$1mk@2950>
2023-11-11 01:44:23 +0300

    1

--- src/test/java/utils/RandomTypeTicket.java
+++ src/test/java/utils/RandomTypeTicket.java
@@ -6,7 +6,7 @@ public class RandomTypeTicket {
     public static String
     standart = randomTypeTicket();
     public static String randomTypeTicket() {
-        String[] standart = {"", "Бизнес", "Первый", "Эконом"};
+        String[] standart = {"Эконом", "Бизнес", "Первый"};
         return new Faker().options().option(standart);
     }
 }
