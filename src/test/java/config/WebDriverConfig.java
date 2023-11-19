0a4c6f823f6c48be220f1714e5552f4a6da01079
Toalra <K0$k@$1mk@2950>
2023-11-20 00:08:03 +0300

    1

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -18,6 +18,7 @@ public class WebDriverConfig {
         Configuration.browserVersion = webConfig.getBrowserVersion();
         Configuration.browserSize = webConfig.getBrowserSize();
         Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000;
 
         if (webConfig.isRemote()) {
             Configuration.remote = webConfig.getRemoteUrl();
