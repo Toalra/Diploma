14a304b105973191a1fb158de50fdac08aa9cadb
Toalra <K0$k@$1mk@2950>
2023-11-19 23:16:12 +0300

    1

--- src/test/java/config/WebDriverProvider.java
+++ src/test/java/config/WebDriverProvider.java
@@ -1,52 +1,38 @@
-//package config;
-//
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import org.aeonbits.owner.ConfigFactory;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//import com.codeborne.selenide.Browser;
-//
-//import java.util.function.Supplier;
-//
-//import static config.Browser.CHROME;
-//import static config.Browser.FIREFOX;
-//
-//public class WebDriverProvider implements Supplier<WebDriver> {
-//    private final WebConfig config;
-//
-//    public WebDriverProvider() {
-//        this.config = ConfigFactory.create(WebConfig.class, System.getProperties());
-//    }
-//    @Override
-//    public WebDriver get() {
-//        WebDriver driver = createDriver();
-//        driver.get(config.getBaseUrl());
-//        return driver;
-//    }
-//    public WebDriver createDriver() {
-//        case "CHROME": {
-//            WebDriverManager.chromedriver().setup();
-//            return new ChromeDriver();
-//        }
-//        case "FIREFOX": {
-//            WebDriverManager.firefoxdriver().setup();
-//            return new FirefoxDriver();
-//        }
-//    }
-////        switch (config.getBrowser()) {
-////            case Browser.CHROME: {
-////                WebDriverManager.chromedriver().setup();
-////                return new ChromeDriver();
-////            }
-////            case Browser.FIREFOX: {
-////                WebDriverManager.firefoxdriver().setup();
-////                return new FirefoxDriver();
-////            }
-////            default: {
-////                throw new RuntimeException("No such driver");
-////            }
-////        }
-////    }
-//
-//}
+package config;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import com.codeborne.selenide.Browser;
+
+import java.util.function.Supplier;
+
+import static config.Browser.CHROME;
+import static config.Browser.FIREFOX;
+
+public class WebDriverProvider implements Supplier<WebDriver> {
+    private final WebConfig config;
+
+    public WebDriverProvider() {
+        this.config = ConfigFactory.create(WebConfig.class, System.getProperties());
+    }
+    @Override
+    public WebDriver get() {
+        WebDriver driver = createDriver();
+        driver.get(config.getBaseUrl());
+        return driver;
+    }
+    public WebDriver createDriver() {
+        Browser browser = config.getBrowser();
+        if (browser.equals(CHROME)) {
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        } else if (browser.equals(FIREFOX)) {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }
+        return null;
+    }
+}
